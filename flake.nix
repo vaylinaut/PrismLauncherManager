a1c3dc5f00f9d1a651837085236cb29a558c5e98
vaylinaut <50423894+vaylinaut@users.noreply.github.com>
2026-08-12 17:28:30 -0400

    Refactor flake.nix to include Home Manager module

--- flake.nix
+++ flake.nix
@@ -3,11 +3,30 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    # Required so your module can be used directly from GitHub
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, ... }: {
-    # home manager
-    nixosModules.prismLauncher = ./nixos.nix;
+  outputs = { self, nixpkgs, home-manager, ... }: {
+    # Export your Home Manager module
     homeManagerModules.prismLauncher = ./home-manager.nix;
+
+    # Optional convenience alias
+    homeManagerModule = self.homeManagerModules.prismLauncher;
+
+    # Provide a test configuration (optional but useful)
+    # Users can run: nix run github:you/prism-launcher#homeConfigurations.example
+    homeConfigurations.example = home-manager.lib.homeManagerConfiguration {
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+
+      modules = [
+        ./home-manager.nix
+        {
+          programs.MCSR-Prismlauncher.enable = true;
+        }
+      ];
+    };
   };
 }
